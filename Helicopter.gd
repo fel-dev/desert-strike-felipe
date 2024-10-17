4e40c54c7ca52dcbdd8e97c23835b96e4cf1d885
Felipe Carneiro <felipecarneiro@duck.com>
2024-10-16 21:18:07 -0300

    WIP uziped

---
+++ Helicopter.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var speed = 200
+ 
+func _process(delta):
+	var direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+		
+	position += direction.normalized() * speed * delta
+"res://Helicopter.gd"
